851f7959d88c5ebd7183665f627d21191a2d666f
Emirhan Çöpoğlu <copogluemirhan@outlook.com>
2024-02-17 14:48:43 +0300

    css updated

--- src/Components/İnformation.js
+++ src/Components/İnformation.js
@@ -11,20 +11,24 @@ const İnformation = () => {
       <div className="info">
         <div className="infoselectcar">
           <img className="carkeyimg" src={carkey} alt="Arabanı Seç"></img>
-          <h4>Arabanı seç</h4>
-          <p>Tüm sürüş ihtiyaçlarınız için geniş araba yelpazesi</p>
+          <h4 className="infoselectcarh">Arabanı seç</h4>
+          <p className="infoselectcarp">
+            Tüm sürüş ihtiyaçlarınız için geniş araba yelpazesi
+          </p>
         </div>
         <div className="infocontactoperator">
           <img src={operator} alt="Müşteri Hizmetleri"></img>
-          <h4>Müşteri Hizmetleri</h4>
-          <p>
+          <h4 className="infoselectcarh">Müşteri Hizmetleri</h4>
+          <p className="infoselectcarp">
             Herhangi bir sorunda deneyimli arkadaşlarımızın yardımı hizmetinizde
           </p>
         </div>
         <div className="carhappines">
           <img src={car} alt="Araç Memnuniyeti"></img>
-          <h4>Araç Memnuniyeti</h4>
-          <p>Araçlarımızın bakımları zamanında yapılmaktadır</p>
+          <h4 className="infoselectcarh">Araç Memnuniyeti</h4>
+          <p className="infoselectcarp">
+            Araçlarımızın bakımları zamanında yapılmaktadır
+          </p>
         </div>
       </div>
     </div>
