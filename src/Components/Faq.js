da2c34d65b4633908673f95ba5a783b234cf7f5d
Emirhan Çöpoğlu <copogluemirhan@outlook.com>
2024-02-27 15:00:03 +0300

    data folder , bug fixed

--- src/Components/Faq.js
+++ src/Components/Faq.js
@@ -1,31 +1,8 @@
 import React, { useState } from "react";
 import "../Styles/FaqStyle.css";
+import accordionData from "../Data/Accordion";
 
 const Faq = () => {
-  const accordionData = [
-    {
-      title: "1. Araç kiralamak için gerekli gereksinimler nelerdir?",
-      content: `Araç Kiralamak için en az 21 yaşını doldurmuş olmanız ve en az 1 yıllık B grubu ehliyet sahibi olmanız gerekmektedir.`,
-    },
-    {
-      title: "2. Araç kiralama süreleri nelerdir?",
-      content: `Araç kiralama minimum süresi günlük kiralamalarda 24 saat, aylık kiralamalarda ise 30 gündür.`,
-    },
-
-    {
-      title: "3. Araç iade süresi nedir?",
-      content: `Araçların iadesinde yaşanan gecikmelerde, sadece 59 dakikaya kadar opsiyon uygulanır.`,
-    },
-    {
-      title: "4. Rezervasyon değişimi yapabilir miyim?",
-      content: `Rezervasyon tarihinizden önce planlarınızda değişik olursa , çağrı merkezini arayabilir veya Canlı Destek üzerinden anında iletişim kurabilirsiniz. Rezervasyon değişikliği için talebinizi iletirken lütfen rezervasyon kodunuzu veya isim soyisim irtibat numaranızı bildiriniz.`,
-    },
-    {
-      title: "5. Rezervasyon oluşturduğum aracı kesinlikle alabiliyor muyum?",
-      content: `Rezervasyon oluşturduğunuz aracı alabilirsiniz, fakat araçlarda oluşan mekanik veya fiziksel hasarlanmalardan kaynaklı problemlerde size sunulan aracın muadili sizlere sunulur ve teslim edilir.`,
-    },
-  ];
-
   const [openIndex, setOpenIndex] = useState(null);
 
   const handleAccordionClick = (index) => {
--- src/Components/WhoWeAre.js
+++ src/Components/WhoWeAre.js
@@ -2,27 +2,29 @@ import React from "react";
 import "../Styles/WhoWeAreStyle.css";
 import companyface1 from "../Assets/companyface1.jpg";
 import withcustomer from "../Assets/withcustomer.jpg";
+import contactData from "../Data/ContactData.json";
 
 const WhoWeAre = () => {
+  const whowearefirst = contactData.ContactData[0].whowearedata;
+  const whowearesecond = contactData.ContactData[0].whowearedatasecond;
+
+  const textSecond =
+    contactData.ContactData[0].whowearedatasecond["text-second"];
+  const lines = textSecond.split("\n");
+  const textSecondWithLi = lines.map((line, index) => (
+    <li key={index}>{line}</li>
+  ));
   return (
     <div className="aboutmaincontainer">
       <div className="aboutcontainer">
         <div className="about">
           <img src={companyface1} alt="Company Face"></img>
           <div className="aboutcontainer-text">
-            <h3>Biz Kimiz?</h3>
+            <h3>{whowearefirst.title}</h3>
             <h5>
-              Rent A Car, müşterilerine güvenilir, konforlu ve özel bir araç
-              kiralama deneyimi sunan öncü bir şirkettir. Rent A Car sektördeki
-              uzmanlığı ve müşteri odaklı hizmet anlayışıyla bilinir. {<br />}
+              {whowearefirst.text} {<br />}
               {<br />}
-              <h5 id="alt-text">
-                Sektörde lider bir araç kiralama markası olma hedefiyle, müşteri
-                beklentilerini aşan kaliteli hizmetler sunarak, sürekli
-                gelişmeyi ve yeniliği benimsemekteyiz. Rent A Car, müşteri
-                odaklı yaklaşımıyla sektörde öncü olmayı sürdürecek ve
-                müşterilerine değer katmaya devam edecektir.
-              </h5>
+              <h5 id="alt-text">{whowearefirst["text-second"]}</h5>
             </h5>
           </div>
         </div>
@@ -31,21 +33,12 @@ const WhoWeAre = () => {
         <div className="about">
           <img src={withcustomer} alt="Customer"></img>
           <div className="aboutcontainer-text">
-            <h3>Gezini şimdi planla</h3>
+            <h3>{whowearesecond.title}</h3>
             <h5>
-              Müşteri memnuniyetini en üst düzeye çıkarmak ve her bir
-              müşterimize özel bir araç kiralama deneyimi sunmak için buradayız.
-              Rent A Car, her müşterisinin ihtiyaçlarına özel çözümler sunarak,
-              seyahatlerini daha keyifli ve sorunsuz hale getirmeyi
-              amaçlamaktadır. {<br />}
+              {whowearesecond.text} {<br />}
               {<br />}
               <h5 id="alt-text">
-                <ul>
-                  <li>Güvenilir ve profesyonel hizmet anlayışı</li>
-                  <li>Geniş ve çeşitli araç filosu</li>
-                  <li>Rekabetçi fiyatlar ve esnek ödeme seçenekleri</li>
-                  <li>7/24 müşteri destek hizmeti</li>
-                </ul>
+                <ul>{textSecondWithLi}</ul>
               </h5>
             </h5>
           </div>
