4af19d9e5e06d220650751e7967814a8ecd46438
Emirhan Çöpoğlu <copogluemirhan@outlook.com>
2024-03-31 17:27:25 +0300

    hamburger menu created

--- src/Components/MobileHeader.js
+++ src/Components/MobileHeader.js
@@ -2,53 +2,74 @@ import React from "react";
 import { useState } from "react";
 import "../Styles/MobileHeaderStyle.css";
 import carrental from "../Assets/carrental.png";
-import { Link } from "react-router-dom";
 
-const MobileHeader = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+const HamburgerHeaderComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className="mobileheadercontainer">
-      <div className="mobileheader-second">
-        <div className="headerlogo">
-          <div className="headersecondlogo">
-            <div className="headerlogo-headertoggle">
-              <a href="/">
-                <img src={carrental} alt="Car Rental Logo"></img>
-              </a>
-              <label
-                for="menu-toggle"
-                className="menu-icon"
-                onClick={() => {
-                  setMenuOpen(!menuOpen);
-                }}
-              >
-                ☰
-              </label>
-            </div>
-          </div>
-          <div className="mobileheaderlinks">
-            <ul className={menuOpen ? "open" : ""}>
-              <Link to={"/"}>
-                <li>Anasayfa </li>{" "}
-              </Link>
-              <Link to={"/about"}>
-                <li>Hakkımızda </li>
-              </Link>
-              <Link to={"/carmodels"}>
-                <li>Araçlar</li>
-              </Link>
-              <Link to={"/team"}>
-                <li>Ekibimiz</li>
-              </Link>
-              <Link to={"/contact"}>
-                <li>İletişim</li>
-              </Link>
-            </ul>
+    <div className="hamburgermenuclass">
+      <header className="App-header">
+        <div className="left-content">
+          <img src={carrental} alt="MobileHeader"></img>
+        </div>
+        <div className="right-content">
+          <div className="menu-toggle" onClick={toggleMenu}>
+            &#9776;
           </div>
         </div>
-      </div>
+        <nav className={`menu ${isOpen ? "open" : ""}`}>
+          <div className="close-button" onClick={closeMenu}>
+            &times;
+          </div>
+          <ul>
+            <li>
+              <a href="/" onClick={closeMenu}>
+                Ana Sayfa
+              </a>
+            </li>
+            <li>
+              <a href="/about" onClick={closeMenu}>
+                Hakkımızda
+              </a>
+            </li>
+            <li>
+              <a href="/carmodels" onClick={closeMenu}>
+                Araç Modelleri
+              </a>
+            </li>
+            <li>
+              <a href="/team" onClick={closeMenu}>
+                Ekibimiz
+              </a>
+            </li>
+            <li>
+              <a href="/contact" onClick={closeMenu}>
+                İletişim
+              </a>
+            </li>
+            <li>
+              <a href="/signup" onClick={closeMenu}>
+                Kaydol
+              </a>
+            </li>
+            <li>
+              <a href="/login" onClick={closeMenu}>
+                Giriş
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 };
 
-export default MobileHeader;
+export default HamburgerHeaderComponent;
